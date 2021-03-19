e4c4d048a196213b4ec03b9c17d8e871e15c5c94
rhammer35 <rhammer35@icloud.com>
2021-03-18 22:58:21 -0400

    created styled header and image components

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 // This function exports an information section component to App.js
 // The information section displays data from the NASA image object about the particular image for a given day
+
+const StyledInfoSection = styled.section `
+  color: 'purple';
+`
+
 export default function Details (props) {
   return (
-    <section className='info-container'>
+    <StyledInfoSection className='info-container'>
       <h2>About This Image</h2>
       <p>{props.explanation}</p>
-    </section>
+    </StyledInfoSection>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,19 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
 // This function exports the header component to App.js
 // The header will contain a title for the App and the current date since the photo of the day changes each day
 
-// const StyledHeader = styled.header`
-//   color: 'purple';
-//   width: 60%;
-//   display: 'flex';
-//   flexDirection: 'row';
-//   alignItems: 'center';
-//   justifyContent: 'space-evenly';
-// `;
+const StyledHeader = styled.header`
+  text-align: center;
+  color: #221C35;
+`
 
 export default function Header (props) {
   return (
-    <header>
+    <StyledHeader>
       <h1>Welcome to the NASA Image of the Day App!</h1>
       <h4> To select a different date, use the input below. You are currently viewing the image for: {props.date}</h4>
-    </header>
+    </StyledHeader>
   );
 }
